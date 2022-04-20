ba2bca13272963d11cbbc648c3fd1e832a53618c
sean3434 <seannyc34@gmail.com>
2022-04-20 13:59:32 -0400

    Array of CRUDable Objects

--- models/videogames.js
+++ models/videogames.js
@@ -5,68 +5,68 @@ module.exports = [
         sales: "238,000,000",
         releaseDate: "November 18, 2011",
         image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Minecraft_cover.png/220px-Minecraft_cover.png"
-    }
+    },
     {
         title: "Grand Theft Auto V",
         platform: "Multi-platform",
         sales: "160,000,000",
         releaseDate: "September 17, 2013",
         image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Grand_Theft_Auto_V.png/220px-Grand_Theft_Auto_V.png"
-    }
+    },
     {
         title: "Tetris",
         platform: "Multi-platform",
         sales: "100,000,000",
         releaseDate:"September 12, 2006",
         image:"http://www.vgmpf.com/Wiki/images/thumb/3/37/Tetris_-_NES_-_Album_Art.jpg/600px-Tetris_-_NES_-_Album_Art.jpg"
-    }
+    },
     {
         title: "Wii Sports",
         platform: "Wii",
         sales: "82,900,000",
         releaseDate: "November 19, 2006",
         image:"https://images.nintendolife.com/games/wii/wii_sports/cover_large.jpg"
-    }
+    },
     {
         title: "PUBG: Battlegrounds",
         platform: "Multi-platform",
         sales: "75,000,000",
         releaseDate: "December 20, 2017",
         image:"https://cdn-products.eneba.com/resized-products/lvoic2oakbklg2dytgpa_350x200_1x-0.jpg"
-    }
+    },
     {
         title: "Super Mario Bros.",
         platform: "Multi-platform",
         sales: "58,000,000",
         releaseDate: "September 13, 1985",
-        image: ""
-    }
+        image: "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
+    },
     {
         title: "Mario Kart 8 / Deluxe",
         platform: "Wii U / Switch",
         sales: "51,810,000",
         releaseDate: "May 29, 2014",
-        image:""
-    }
+        image:"https://s3.gaming-cdn.com/images/products/2615/orig-fallback-v1/mario-kart-8-deluxe-switch-switch-game-nintendo-eshop-europe-cover.jpg"
+    },
     {
         title: "Pokémon Red / Green / Blue / Yellow",
         platform: "Game Boy / Color",
         sales: "47,520,000",
         releaseDate: "February 27, 1996",
-        image: ""
-    }
+        image: "https://www.giantbomb.com/a/uploads/scale_medium/1/17172/1226087-pkmn_rab.jpg"
+    },
     {
         title: "Wii Fit / Plus",
         platform: "Wii",
         sales: "43,800,000",
         releaseDate: "December 1, 2007",
-        image: ""
-    }
+        image: "https://images.nintendolife.com/games/wii/wii_fit_plus/cover_large.jpg"
+    },
     {
         title: "Red Dead Redemption 2",
         platform: "Multi-platform",
         sales: "43,000,000",
         releaseDate: "October 26, 2018",
-        image: ""
+        image: "https://img.g2a.com/1080x1080/1x1x0/red-dead-redemption-2-rockstar-key-global/5da85be37e696c2b755a1612"
     }
 ]
